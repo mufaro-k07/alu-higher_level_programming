0336bf292dbd78f9cb8fbb0aad01e24536f0bedf
mufaro-k07 <m.kunze@alustudent.com>
2025-06-27 12:48:23 -0700

    Final touch ups

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(c) for c in range(97, 123) if c != 101 and c != 113)), end="")
+print("".join(
+    [chr(i) for i in range(97, 123) if i != 101 and i != 113]
+), end="")
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     result = ""
     for c in str:
         if 97 <= ord(c) <= 122:
-            result += chr(ord(c) -32)
+            result += chr(ord(c) - 32)
         else:
             result += c
     print("{}".format(result))
