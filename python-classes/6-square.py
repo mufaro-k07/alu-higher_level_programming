740743ca79a601f4d28813b55d6233bb4755b551
mufaro-k07 <m.kunze@alustudent.com>
2025-07-10 07:07:21 -0700

    Coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with size, position, area calculation,
+and a custom printing method.
+"""
+
+
+class Square:
+    """A class that defines a square by its size and position."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialise a new Square instance.
+
+        Args:
+            size (int): The size of square (default is 0).
+            position (tuple): The position offset (default is (0, 0)).
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square."""
+        return self.___size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the current position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self ,value):
+        """Set the position with validation."""
+        if (
+            not isinstance(value, tuple) or 
+            len(value) != 2 or
+            not all(isinstance(n, int) for n in value) or 
+            not all(n >= 0 for n in value)
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Print the square using the '#' character.
+        Uses the position to define spaces and newlines.
+        """
+        if self.__size == 0:
+            print()
+            return
+
+        # Print vertical spacing
+        for _ in range(self.__position[1]):
+            print()
+
+        # Print each line of the square with horizontal spacing
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
