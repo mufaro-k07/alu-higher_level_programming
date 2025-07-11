65a892e75cd066f207147b55a87d5fba5fc2065e
mufaro-k07 <m.kunze@alustudent.com>
2025-07-11 05:42:28 -0700

    Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns the list of available \
+        attributes and methods of an object.
+"""
+
+
+def loopuk(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+    """
+    return dir(obj)
