f515f12a454ec8ae4d27f5183c743bf37a1c55f6
mufaro-k07 <m.kunze@alustudent.com>
2025-07-14 05:05:02 -0700

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines a function that returns the list 
+This module defines a function that returns the list
 of available attributes and methods of an object.
 """
 
