b0701bfca30acf5112c6a8bef23098b4b110bb5f
mufaro-k07 <m.kunze@alustudent.com>
2025-07-17 06:09:35 -0700

    4. First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Script that creates a table called first_table in current database in MySQL
+-- first_table with id and name columns
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
