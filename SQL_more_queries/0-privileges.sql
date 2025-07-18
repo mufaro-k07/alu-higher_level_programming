427d72680106795bdbb905bb63f8e4c0ffff98ba
mufaro-k07 <m.kunze@alustudent.com>
2025-07-18 11:55:51 -0700

    0. My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List all privileges of user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List all privileges of user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
