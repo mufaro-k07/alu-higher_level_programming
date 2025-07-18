629930f01e1192d03df20b673d7451cc2274e7f9
mufaro-k07 <m.kunze@alustudent.com>
2025-07-18 12:03:47 -0700

    1. Root user

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on everything to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
